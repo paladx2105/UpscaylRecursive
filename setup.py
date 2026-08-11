c5ce140143ebd7aec391270e403fae21ce7c0b4d
paladx2105 <tobias.schoenfelder@outlook.de>
2026-08-11 17:01:35 +0000

    Changes to be committed: 	modified:   setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='paladx2105',
+    version='0.0.1',
+    description='Better Upscayl GUI for recursive directory upscayling',
+    url='https://github.com/paladx2105/UpscaylRecursive.git',
+    author='Tobias Schönfelder',
+    author_email='',
+    license='GNU GENERAL PUBLIC LICENSE v.3',
+    packages=['pyside6'],
+    zip_safe=False
+)
